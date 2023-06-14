6ba47f1d731e2416bdd0bb55f992f16eb8c32379
PierluigiR <pierapacci@gmail.com>
2023-06-14 17:48:57 -0500

    Creacion de páginas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import 'semantic-ui-css/semantic.min.css';
+import { Button, Icon, Menu } from 'semantic-ui-react';
 import './App.css';
+import Home from "./pages/Home"
+import Comics from "./pages/Comics"
+import Series from "./pages/Series"
+import Error404 from "./pages/Error404"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header-menu">
+        <Menu secondary>
+            <Menu.Item name="inicio" onClick={()=>console.log("haciendo clic en el menu")} />
+            <Menu.Item name="series" onClick={()=>console.log("haciendo clic en series")} />
+            <Menu.Item name="comics" onClick={()=>console.log("haciendo clic en comics")} />
+        </Menu>
+    </div>
     </div>
   );
 }
 
 export default App;
+
+
