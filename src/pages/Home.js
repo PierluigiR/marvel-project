632f069600220484d23c7b251bfe99fe591b46fc
PierluigiR <pierapacci@gmail.com>
2023-06-14 18:47:42 -0500

    Import Header en Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import Header from "./components/header/Header.js";
+
+export default function Home() {
+    return (
+        <div>
+            <p>Estamos en Home</p>
+            <Header />
+        </div>
+    );
+}
