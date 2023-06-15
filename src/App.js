36de2cd13eadd79c43c5b51c6827b11e8d0a8408
PierluigiR <pierapacci@gmail.com>
2023-06-15 12:06:32 -0500

    Creacion de BestCharacters.scss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import 'semantic-ui-css/semantic.min.css';
 import { Button, Icon, Menu } from 'semantic-ui-react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Comics from "./pages/Comics";
 import Series from "./pages/Series";
 import Error404 from "./pages/Error404";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={() => <Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/series" element={<Series />} />
-          <Route path="/comics" element={<Comics />} />
-          <Route path="*" element={<Error404 />} />
-        </Routes>  
-      </BrowserRouter>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={() => <Home />} />
+          <Route path="/inicio" exact component={() => <Home />} />
+          <Route path="/series" exact component={() => <Series />} />
+          <Route path="/comics" exact component={() => <Comics />} />
+          <Route component={() => <Error404 />} />
+        </Switch>
+      </Router>  
+     
       
     </div>
   );
