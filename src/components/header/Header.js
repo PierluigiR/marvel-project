836acf6bae24ebcf18e00db27137dbe35fa05da2
PierluigiR <pierapacci@gmail.com>
2023-06-14 20:06:29 -0500

    UseLocation and UseHistory

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,25 @@
 import './Header.scss';
 import { Menu } from 'semantic-ui-react';
+import { useState } from 'react';
+import { useHistory, useLocation } from 'react-router';
 
 export default function Header() {
+    const currentPath = useLocation();
+    console.log(currentPath);
+    const finalCurrentPath = currentPath.pathname.replace("/", "");
+    const {activeItem, setActiveItem} =useState("");
+    const history = useHistory();
+    const itemClick = (e, {name}) => {
+        setActiveItem (name);
+        history.push(name);
+    };
+    
     return(
         <div className='header-menu'>
             <Menu secondary>
-                <Menu.Item name='Inicio' onClick={()=> console.log("haciendo click en el menu")} />
-                <Menu.Item name='Comics' onClick={()=> console.log("haciendo click en el comics")} />
-                <Menu.Item name='Series' onClick={()=> console.log("haciendo click en el series")} />
+                <Menu.Item name='Inicio' active={activeItem==="Inicio"} onClick={itemClick} />
+                <Menu.Item name='Comics' active={activeItem==="Comics"}onClick={itemClick} />
+                <Menu.Item name='Series' active={activeItem==="Series"}onClick={itemClick} />
             </Menu>
         </div>
     );
