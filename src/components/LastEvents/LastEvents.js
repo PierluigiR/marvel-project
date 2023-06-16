554c209017c87a2486286dba68ac8949e6cb2517
PierluigiR <pierapacci@gmail.com>
2023-06-16 17:41:59 -0500

    Cambios en ListEvents.js

--- src/components/LastEvents/LastEvents.js
+++ src/components/LastEvents/LastEvents.js
@@ -1,6 +1,17 @@
+import { Card, Icon, Image, Dimmer, Loader, Button } from "semantic-ui-react";
+import "./LastEvents.scss";
+
 export default function LastEvents({lastEventsFetch}) {
-    console.log(lastEventsFetch);
+    const {loading, result} = lastEventsFetch;
+    if (loading || !result) 
+        return(
+            <Dimmer active inverted>
+                <Loader inverted>Loading...</Loader>
+            </Dimmer>
+        );
+    const {results}= result.data;
+    console.log(results);
     return(
-        <div>Desde Last Events</div>
+            <div>DesdeLastEvents</div>
     );
 }
--- src/components/ListLastEvent/ListLastEvent.js
+++ src/components/ListLastEvent/ListLastEvent.js
@@ -4,10 +4,8 @@ import "./ListLastEvent.scss";
 import useFetch from "../Hooks/useFetch";
 import LastEvents from "../LastEvents/LastEvents";
 
-    //const lastEventFetch= useFetch(`${process.env.REACT_APP_URL_BASE}`)
-    //console.log(process.env.REACT_APP_URL_BASE);
 export default function ListLastEvent() {
-    const lastEventsFetch = useFetch('http://gateway.marvel.com/v1/public/comics?ts=1&apikey=952b42a104ba53bca224cbe292cb0948&hash=13045efc8851c7dcf8beca0aeb852c33');
+    const lastEventsFetch = useFetch('http://gateway.marvel.com/v1/public/comics?ts=1&apikey=952b42a104ba53bca224cbe292cb0948&hash=13045efc8851c7dcf8beca0aeb852c33&limit=5');
     console.log(lastEventsFetch);
     return (
         <div className="container-list-last-events">
