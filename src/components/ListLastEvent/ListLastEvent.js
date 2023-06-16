4c8e188db7cebc1b3123bcad690bbc8df06a095c
PierluigiR <pierapacci@gmail.com>
2023-06-16 10:51:30 -0500

    Cambios en ListLastEvent

--- src/components/ListLastEvent/ListLastEvent.js
+++ src/components/ListLastEvent/ListLastEvent.js
@@ -2,9 +2,12 @@ import { Card, Header } from "semantic-ui-react";
 import Container from "../Container/Container";
 import "./ListLastEvent.scss";
 import useFetch from "../Hooks/useFetch";
-export default function ListLastEvent() {
+
     //const lastEventFetch= useFetch(`${process.env.REACT_APP_URL_BASE}`)
-    console.log(process.env.REACT_APP_URL_BASE);
+    //console.log(process.env.REACT_APP_URL_BASE);
+export default function ListLastEvent() {
+    const lastEventsFetch = useFetch('http://gateway.marvel.com/v1/public/comics?ts=1&apikey=952b42a104ba53bca224cbe292cb0948&hash=13045efc8851c7dcf8beca0aeb852c33');
+    console.log(lastEventsFetch);
     return (
         <div className="container-list-last-event">
             <Header size="large">Ultimos eventos</Header>
