538d38ab35a19d6327a5e38ad06a81061c855584
PierluigiR <pierapacci@gmail.com>
2023-06-16 21:55:03 -0500

    Ajustes en LastEvents.js y llamado de ListLastEvent

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import BestCharacters from "../components/BestCharacters";
+import ListLastEvent from "../components/ListLastEvent/ListLastEvent";
 
 export default function Home() {
     return (
         <div>
            <BestCharacters />
+           <ListLastEvent />
             
         </div>
     );
