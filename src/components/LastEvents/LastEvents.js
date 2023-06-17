a26fc685002f78759080ef9ae9b3e5f9100cbc6a
PierluigiR <pierapacci@gmail.com>
2023-06-16 22:14:18 -0500

    Propiedades del Button en LastEvents.js

--- src/components/LastEvents/LastEvents.js
+++ src/components/LastEvents/LastEvents.js
@@ -17,6 +17,29 @@ export default function LastEvents({lastEventsFetch}) {
                 <Image
                     src={`${event.thumbnail.path}.${event.thumbnail.extension}`}
                     wrapped ui={false}/>
+                <Card.Content>
+                    <Card.Header>{event.title}</Card.Header>
+                    <Card.Meta>
+                        <span>
+                            <Icon name="book" />
+                            {event.comics} Comics
+                        </span>
+                    </Card.Meta>
+                    <Card.Description>
+                        {event.description}
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <Button
+                    animated
+                    fluid
+                    as="a"
+                    href={event.urls[0].url}
+                    target="_blank"
+                    color="black"
+                    >
+                    </Button>
+                </Card.Content>
             </Card>
         )); 
 }
