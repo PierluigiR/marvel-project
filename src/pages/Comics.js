38b5146bf70b6f162f984a23e6c2661f4cdbe310
PierluigiR <pierapacci@gmail.com>
2023-06-20 11:24:05 -0500

    Cambios en Series.js y Comics.js

--- src/pages/Comics.js
+++ src/pages/Comics.js
@@ -7,7 +7,7 @@ import useFetch from "../Hooks/useFetch";
 export default function Comics() {
     const [renderComics, setRenderComics]= useState(10);
     
-    const ListComics= useFetch('http://gateway.marvel.com/v1/public/comics?ts=1&apikey=952b42a104ba53bca224cbe292cb0948&hash=13045efc8851c7dcf8beca0aeb852c33&limit=${renderComics}');
+    const listComics= useFetch('http://gateway.marvel.com/v1/public/comics?ts=1&apikey=952b42a104ba53bca224cbe292cb0948&hash=13045efc8851c7dcf8beca0aeb852c33&limit=${renderComics}');
     console.log(ListComics);
     return(
         <div className="comics-page">
@@ -16,6 +16,11 @@ export default function Comics() {
                 <Grid.Column>
                     <Container bg="light">
                         <Header as="h2">Los mejores Comics</Header>
+                        <ListComics 
+                            listComics={listComics}
+                            renderComics={renderComics}
+                            setRenderComics={setRenderComics}
+                        />
                     </Container>
                 </Grid.Column>
             </Grid>
