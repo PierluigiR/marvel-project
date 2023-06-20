9c339f6e503e595254f1b7e2f73ec4977511fe05
PierluigiR <136016917+PierluigiR@users.noreply.github.com>
2023-06-20 12:00:58 -0500

    Comics.js

---
+++ src/pages/Comics.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Grid, Header } from "semantic-ui-react";
+import Container from "../components/Container";
+import ListComics from "../components/ListComics";
+import useFetch from "../Hooks/useFetch";
+
+export default function Comics() {
+    const [renderComics, setRenderComics]= useState(10);
+    
+    const listComics= useFetch('http://gateway.marvel.com/v1/public/comics?ts=1&apikey=952b42a104ba53bca224cbe292cb0948&hash=13045efc8851c7dcf8beca0aeb852c33&limit=${renderComics}');
+    console.log(ListComics);
+    return(
+        <div className="comics-page">
+            <div id="slide-comics-image">
+            <Grid>
+                <Grid.Column>
+                    <Container bg="light">
+                        <Header as="h2">Los Comics más destacados</Header>
+                        <ListComics 
+                            listComics={listComics}
+                            renderComics={renderComics}
+                            setRenderComics={setRenderComics}
+                        />
+                    </Container>
+                </Grid.Column>
+            </Grid>
+            </div>
+        </div>
+    );
+}
