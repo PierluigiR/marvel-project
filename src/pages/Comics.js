e5c0cc6c38ecd94bda4480208efdde8b1c4c78c3
PierluigiR <pierapacci@gmail.com>
2023-06-20 19:28:49 -0500

    Cambios en Comics.js

--- src/pages/Comics.js
+++ src/pages/Comics.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from "react";
 import { Grid, Header } from "semantic-ui-react";
 import Container from "../components/Container";
@@ -28,35 +27,4 @@ export default function Comics() {
             </div>
         </div>
     );
-=======
-import { useState } from "react";
-import { Grid, Header } from "semantic-ui-react";
-import Container from "../components/Container";
-import ListComics from "../components/ListComics";
-import useFetch from "../Hooks/useFetch";
-
-export default function Comics() {
-    const [renderComics, setRenderComics]= useState(10);
-    
-    const listComics= useFetch(`http://gateway.marvel.com/v1/public/comics?ts=1&apikey=952b42a104ba53bca224cbe292cb0948&hash=13045efc8851c7dcf8beca0aeb852c33&limit=${renderComics}`);
-    console.log(ListComics);
-    return(
-        <div className="comics-page">
-            <div id="slide-comics-image">
-            <Grid>
-                <Grid.Column>
-                    <Container bg="light">
-                        <Header as="h2">Los Comics más destacados</Header>
-                        <ListComics 
-                            listComics={listComics}
-                            renderComics={renderComics}
-                            setRenderComics={setRenderComics}
-                        />
-                    </Container>
-                </Grid.Column>
-            </Grid>
-            </div>
-        </div>
-    );
->>>>>>> 19f15d1c50554256d3577310cb9163af1a662743
 }
