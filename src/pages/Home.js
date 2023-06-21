e97e3687b42fa570bb3cc0f7d1f0986379e45787
PierluigiR <pierapacci@gmail.com>
2023-06-20 23:07:53 -0500

    Creación del componente Insider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import BestCharacters from "../components/BestCharacters";
 import ListLastEvent from "../components/ListLastEvent/ListLastEvent";
+import Insider from "../components/Insider/Insider";
 
 export default function Home() {
     return (
         <div>
            <BestCharacters />
            <ListLastEvent />
+           <Insider />
             
         </div>
     );
